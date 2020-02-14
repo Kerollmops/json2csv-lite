61204b30ee5db4b63be2c80a4d31bea8194688ca
Clément Renault <renault.cle@gmail.com>
2020-02-14 22:23:00 +0100

    Make it work with jsonlines only

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
-use std::{env, io};
-use std::fs::File;
+use std::io;
+use std::borrow::Cow;
 
-use serde_json::{map::Map, Value, from_slice};
 use main_error::MainError;
-use memmap::Mmap;
+use serde_json::{map::Map, de::IoRead, Value, StreamDeserializer};
+
+fn value_to_string(value: &Value) -> Option<Cow<str>> {
+    match value {
+        Value::Null => None,
+        Value::Bool(b) => Some(Cow::Owned(b.to_string())),
+        Value::Number(n) => Some(Cow::Owned(n.to_string())),
+        Value::String(s) => Some(Cow::Borrowed(s.as_ref())),
+        Value::Array(_) => None,
+        Value::Object(_) => None,
+    }
+}
 
 fn main() -> Result<(), MainError> {
-    let filename = env::args().nth(1).expect("json filename missing");
-    let file = File::open(filename)?;
-    let memory = unsafe { Mmap::map(&file)? };
+    let stdin = IoRead::new(io::stdin());
+    let mut stream = StreamDeserializer::<_, Map<String, Value>>::new(stdin).peekable();
 
-    let array: Vec<Map<String, Value>> = from_slice(&memory)?;
-    let fields: Vec<String> = array[0].keys().cloned().collect();
+    let first = stream.peek().unwrap().as_ref().unwrap().clone();
+    let fields: Vec<_> = first.keys().collect();
 
     let mut wtr = csv::Writer::from_writer(io::stdout());
     wtr.write_record(&fields)?;
 
-    for object in array {
+    for object in stream {
+        let object = object?;
         for field in &fields {
-            match object.get(field).and_then(|v| v.as_str()) {
-                Some(string) => wtr.write_field(string)?,
+            match object.get(field.as_str()).and_then(value_to_string) {
+                Some(string) => wtr.write_field(string.as_ref())?,
                 None => wtr.write_field("")?,
             }
         }
